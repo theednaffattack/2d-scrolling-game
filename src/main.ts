70396643896815d8bc7c2f121826dddc3d9111f9
Eddie Naff <eddie@eddienaff.dev>
2022-05-30 11:26:23 -0700

    Add canvas context selection to setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
-import './style.css'
+export const canvas = document.querySelector("canvas");
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const context = canvas?.getContext("2d");
+console.log(context);
