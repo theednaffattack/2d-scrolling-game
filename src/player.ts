a48d0d7ba538a108c5b531f91f9cf15115dbae70
Eddie Naff <eddie@eddienaff.dev>
2022-05-30 12:14:10 -0700

    Add update function to animate player

--- src/player.ts
+++ src/player.ts
@@ -1,11 +1,23 @@
 type XPosition = number;
 type YPosition = number;
+type XDir = -1 | 0 | 1;
+type YDir = -1 | 0 | 1;
 type Coords = { x: XPosition; y: YPosition };
+interface Vector {
+  x: XDir;
+  y: YDir;
+}
+
+interface Velocity {
+  x: number;
+  y: number;
+}
 
 export class Player {
   color: string;
   height: number;
   position: Coords;
+  velocity: Velocity;
   width: number;
 
   constructor() {
@@ -15,11 +27,20 @@ export class Player {
       x: 100,
       y: 100,
     };
+    this.velocity = {
+      x: 0,
+      y: 3,
+    };
     this.width = 30;
   }
 
   draw(context: CanvasRenderingContext2D) {
     context.fillStyle = this.color;
     context.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update(context: CanvasRenderingContext2D) {
+    this.draw(context);
+    this.position.y += this.velocity.y;
   }
 }
