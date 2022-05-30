0aafa013aa467501c4b1f106ec87e4b596ac4d22
Eddie Naff <eddie@eddienaff.dev>
2022-05-30 11:57:17 -0700

    Make canvas fill the window

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
+import { Player } from "./player";
+
 export const canvas = document.querySelector("canvas");
 
+if (!canvas) {
+  throw new Error("Canvas object is missing!");
+}
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 const context = canvas?.getContext("2d");
-console.log(context);
+
+const player = new Player();
+
+if (!context) {
+  throw new Error("Context object is missing!");
+}
+const drawnPlayer = player.draw(context);
