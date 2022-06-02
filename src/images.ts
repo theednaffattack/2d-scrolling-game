eddc598c6a8fe3a4ef14f290121ae31f93db8ab6
Eddie Naff <eddie@eddienaff.dev>
2022-06-01 17:23:42 -0700

    Add new platform images (taller ones)

--- src/images.ts
+++ src/images.ts
@@ -2,8 +2,10 @@ import { newImage } from "./new-image";
 import hills from "../assets/hills.png";
 import platform from "../assets/platform.png";
 import background from "../assets/background.png";
+import platformSmallTallImage from "../assets/platformSmallTall.png";
 
 export const platformImage = newImage(platform);
 
 export const hillImage = newImage(hills);
 export const backgroundImage = newImage(background);
+export const platformSmallTall = newImage(platformSmallTallImage);
